25de1daeb4718cf73af051eab9cb211db16b2312
Paul Liu <pollux.liu@msn.com>
2017-08-28 12:25:23 +0800

    servicebus

---
+++ SBClient-1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SBClient_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var connectionString = "Endpoint=sb://azure-ipass-solution-servicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=51FKyKqSE5mWH79NcozZHumg5utfHGYR/iG+Ju8UdGU=";
+            var queueName = "logicappstestqueue";
+
+            var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+            var message = new BrokeredMessage(this.textBox1.Text);
+
+            client.Send(message);
+        }
+    }
+}
